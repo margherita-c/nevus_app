b3c2791ef364f89203ef5a4741cf1cb835e6e06c
margherita-c <margherita.cignoni@gmail.com>
2025-06-24 13:14:41 +0200

    removed counter

---
+++ doc/README.md
@@ -0,0 +1,11 @@
+# nevus.app
+Nevus is an app whose purpose is to help people check and track the status of their moles. It allows to: 
+- take pictures and save them on a private gallery accessible only trough the app, so that they aren't stored in public places
+- identify each mole and create a history of evolution of every mole with all the pictures that Nevus has
+- place the moles on a human figure, so that its location will be known even without a photo of it
+## Screens
+### login
+### add an account
+### home
+### camera
+### gallery
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,14 +12,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  
   void _openCamera() {
     Navigator.push(
       context,
@@ -45,11 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
             const SizedBox(height: 40),
             ElevatedButton.icon(
               onPressed: _openCamera,
@@ -72,11 +60,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
