833786c6ba100e3be179dcdaa32f8bbf390fa8fb
margherita-c <margherita.cignoni@gmail.com>
2025-08-12 12:06:43 +0200

    Create campaign.dart

---
+++ lib/models/campaign.dart
@@ -0,0 +1,22 @@
+class Campaign {
+  String id;
+  DateTime date;
+  List<String> photoIds; // List of photo IDs in this campaign
+  
+  Campaign({
+    required this.id,
+    required this.date,
+    List<String>? photoIds,
+  }) : photoIds = photoIds ?? [];
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'date': date.toIso8601String(),
+    'photoIds': photoIds,
+  };
+
+  factory Campaign.fromJson(Map<String, dynamic> json) => Campaign(
+    id: json['id'],
+    date: DateTime.parse(json['date']),
+    photoIds: List<String>.from(json['photoIds'] ?? []),
+  );
